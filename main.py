8ea65261f2da582fa416931d1a0ee40a0fbf6a65
Brennan McGowan <mcgowan.brennan@gmail.com>
2022-12-12 08:36:33 -0500

    cleanup
    removed some unneeded print statements
    
    trimmed down requirements.txt

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import yfinance as yf
 from dotenv import load_dotenv
 import os
 from supabase import create_client, Client
+
 load_dotenv()
 url = os.environ.get("SUPABASE_URL")
-key= os.environ.get("SUPABASE_KEY")
+key = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 finnhub_client = finnhub.Client(api_key="ce8klkaad3i1v8pn82c0ce8klkaad3i1v8pn82cg")
 app = Flask(__name__)
@@ -22,7 +23,6 @@ def index():
     print(data)
     data_formatted = format_data(data.get("earningsCalendar"))
     for ticker in data_formatted:
-        print(ticker.__str__())
         data = supabase.table("Earnings").insert({"ticker": ticker.ticker if ticker.ticker is not None else "",
                                                   "eps_est": ticker.eps_est if ticker.eps_est is not None else 0,
                                                   "rev_est": ticker.rev_est if ticker.rev_est is not None else 0,
@@ -33,17 +33,19 @@ def index():
                                                   "date": ticker.date if ticker.date is not None else ""}).execute()
     return "hello wewda!"
 
+
 @app.route("/getLargestEarnings")
 def getLargestEarnings():
-    print(flask.__version__)
     compiled_earnings = {}
-    for i in range (0,5):
+    for i in range(0, 5):
         day_dict = {}
-        premarket = supabase.table("Earnings").select("*").eq("date", str(i)).eq("hour", "bmo").order("cap", desc=True).limit(10).execute()
-        after_hours = supabase.table("Earnings").select("*").eq("date", str(i)).eq("hour", "amc").order("cap", desc=True).limit(10).execute()
-        all_sessions = []
-        all_sessions.append(premarket.data)
-        all_sessions.append(after_hours.data)
+        premarket = supabase.table("Earnings").select("*").eq("date", str(i)).eq("hour", "bmo").order("cap",
+                                                                                                      desc=True).limit(
+            10).execute()
+        after_hours = supabase.table("Earnings").select("*").eq("date", str(i)).eq("hour", "amc").order("cap",
+                                                                                                        desc=True).limit(
+            10).execute()
+        all_sessions = [premarket.data, after_hours.data]
         if premarket.data[0].get("date") == "0":
             day_dict["Monday"] = all_sessions
             compiled_earnings[0] = day_dict
@@ -59,24 +61,23 @@ def getLargestEarnings():
         elif premarket.data[0].get("date") == "4":
             day_dict["Friday"] = all_sessions
             compiled_earnings[4] = day_dict
-        #print(premarket.data[0])
-    #print(compiled_earnings)
     return compiled_earnings
 
 
 class EarningsData:
-  def __init__(self, ticker, eps_est, rev_est, eps_act, rev_act, hour, cap, date):
-    self.ticker = ticker
-    self.eps_est = eps_est
-    self.rev_est = rev_est
-    self.eps_act = eps_act
-    self.rev_act = rev_act
-    self.hour = hour
-    self.cap = cap
-    self.date = date
-  def __str__(self):
-      return str(self.date) + str(self.ticker) + str(self.cap) + str(self.hour) + str(self.rev_act) + str(self.eps_act) + str(self.eps_est) + str(self.rev_est)
+    def __init__(self, ticker, eps_est, rev_est, eps_act, rev_act, hour, cap, date):
+        self.ticker = ticker
+        self.eps_est = eps_est
+        self.rev_est = rev_est
+        self.eps_act = eps_act
+        self.rev_act = rev_act
+        self.hour = hour
+        self.cap = cap
+        self.date = date
 
+    def __str__(self):
+        return str(self.date) + str(self.ticker) + str(self.cap) + str(self.hour) + str(self.rev_act) + str(
+            self.eps_act) + str(self.eps_est) + str(self.rev_est)
 
 
 def format_data(ticker_info):
@@ -95,7 +96,8 @@ def format_data(ticker_info):
         print(ticker_data.info.get("marketCap"))
         year, month, day = (int(x) for x in ticker.get("date").split('-'))
         day_of_week = datetime.date(year, month, day).weekday()
-        obj = EarningsData(ticker.get("symbol"), ticker.get("epsEstimate"), ticker.get("revenueEstimate"), ticker.get("epsActual"),
+        obj = EarningsData(ticker.get("symbol"), ticker.get("epsEstimate"), ticker.get("revenueEstimate"),
+                           ticker.get("epsActual"),
                            ticker.get("revenueActual"), ticker.get("hour"),
                            ticker_data.info.get("marketCap"),
                            day_of_week)
@@ -105,7 +107,3 @@ def format_data(ticker_info):
             list_of_ticker_objs.append(obj)
     print(len(list_of_ticker_objs))
     return list_of_ticker_objs
-
-
-
-
